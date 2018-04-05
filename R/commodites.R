145cb629f1565d6994311945b40a2ac07582e0db
XavierPrudent <prudentxavier@gmail.com>
2018-04-05 11:31:53 -0400

    first commit

---
+++ R/commodites.R
@@ -0,0 +1,352 @@
+
+#######################################
+## Carte du Saguenay 
+#coord <<- geocode('Saguenay')
+coord <<- data.frame(lon=-71.06849,lat=48.42805)
+#sg.map <<- get_map(c(lon=coord$lon, lat=coord$lat),zoom = 9, maptype = "terrain", source = "stamen")
+
+#######################################
+## Carte des secteurs municipaux
+sm <<- readOGR(dsn="/Users/lavieestuntoucan/Civilia/projets/Saguenay/data/enquete OD 2015/SM21_Sag_2015.TAB",verbose=FALSE)
+sm <<- spTransform(sm, CRS("+proj=longlat +datum=WGS84"))
+sm <<- fortify(sm)
+sm$group <- as.numeric(sm$group)
+
+#######################################
+## Motorised transports
+motor <<- c(1, 2, 3, 4, 6, 7, 8, 10, 11, 12)
+
+#######################################
+## Colors
+blue.sg <<- "#2B4591"
+green.sg <<- "#49842A"
+red.sg <<- "#B82432"
+myPalette <<- colorRampPalette(brewer.pal(10, "Spectral"))
+
+#######################################
+## Plot une matrice
+odmat.plot <- function(mat,title){
+  min.val <- min(mat$freq,na.rm=T)
+  max.val <- max(mat$freq,na.rm=T)
+  med.val <- (min.val + max.val)/2
+  
+  #mat$freq <-round(mat$freq)
+  
+  g <- ggplot(data = mat, aes(x=smdes, y=smori, fill = freq)) +
+    geom_tile(color = "white") +
+    geom_text(aes(label = freq),size=2.5) +
+    scale_fill_gradient2(low = "blue", high = "red", mid = "orange", 
+                         midpoint = med.val, limit = c(min.val,max.val), space = "Lab", 
+                         name="# Dépl.") +
+    theme_minimal() + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 1,size = 8, hjust = 1),
+          axis.text.y = element_text(angle = 0, vjust = 1,size = 8, hjust = 1)) +
+    coord_fixed() + 
+    labs(x="Destination",y="Origine") +
+    ggtitle(title)
+  
+  ## Turn into plotly
+  ggplotly(g,width=750,height=750,tooltip = c("x","y","freq"))
+  # scale_x_continuous(breaks=seq(1:sm.n) ) +
+  #   scale_y_continuous(breaks=seq(1:sm.n) ) +
+  #   
+}
+
+#######################################
+## Cree et plot les matrices
+odmat.cree.plot <- function(data, title, list.sm){
+  ## Cree la matrice
+  sm.mat <- data %>% 
+    group_by(smori,smdes) %>% 
+    dplyr::summarise(freq=round(sum(facdep))) %>%
+    as.data.frame()
+  #  sm.mat <- sm.mat %>% mutate(freq = ifelse(smori == smdes,NA,freq))
+  
+  ## Pour les déplacements manquants, mettre à zéro
+  for( i in list.sm ){
+    for( j in list.sm ){
+      nij <- sm.mat %>% filter(smori==i & smdes==j) %>% nrow()
+      if( nij  == 0 ){
+        sm.ij <- data.frame(smori=i,smdes=j,freq=0)
+        sm.mat <- rbind(sm.mat,sm.ij) 
+      }
+    }
+  }
+  
+  ## Matrix complète
+  odmat.plot(sm.mat, title)
+}
+#####
+#######################################
+## Plot une matrice
+w.odmat.plot <- function(mat,title,leg,dg){
+  min.val <- min(mat$w.val,na.rm=T)
+  max.val <- max(mat$w.val,na.rm=T)
+  med.val <- (min.val + max.val)/2
+  
+  ggplot(data = mat, aes(x=smdes, y=smori, fill = w.val)) +
+    geom_tile(color = "white") +
+    geom_text(aes(label = round(w.val,digits=dg)),size=2.5) +
+    scale_fill_gradient2(low = "blue", high = "red", mid = "orange", 
+                         midpoint = med.val, limit = c(min.val,max.val), space = "Lab", 
+                         name=leg) +
+    theme_minimal() + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 1,size = 8, hjust = 1),
+          axis.text.y = element_text(angle = 0, vjust = 1,size = 8, hjust = 1)) +
+    coord_fixed() + 
+    labs(x="Destination",y="Origine") +
+    ggtitle(title)
+}
+  
+#######################################
+## Cree et plot les matrices avec moyennes ponderées
+w.odmat.cree.plot <- function(sm.mat, title, leg, list.sm,dg){
+  
+  ## Pour les déplacements manquants, mettre à zéro
+  for( i in list.sm ){
+    for( j in list.sm ){
+      nij <- sm.mat %>% filter(smori==i & smdes==j) %>% nrow()
+      if( nij  == 0 ){
+        sm.ij <- data.frame(smori=i,smdes=j,w.val=NA)
+        sm.mat <- rbind(sm.mat,sm.ij) 
+      }
+    }
+  }
+  
+  ## Matrix complète
+  w.odmat.plot(sm.mat, title, leg,dg)
+}
+
+
+#######################################
+## Plot un histo de facteurs
+plot.hist.fact <- function(data, x.lab, x.var, w){
+  ggplot(data,aes_string(x=x.var, weight = w)) + 
+    geom_histogram(fill=blue.sg, stat = "count")  + 
+    xlab(x.lab) +
+    ylab("Nombre de personnes") 
+}  
+
+#######################################
+## Plot un histo de numeriques
+plot.hist.num <- function(data, x.lab, x.var, w, bins){
+  ggplot(data,aes_string(x=x.var, weight = w)) + 
+    geom_histogram(fill=blue.sg)  + 
+    xlab(x.lab) +
+    ylab("Nombre de personnes") +
+    scale_x_continuous(breaks=bins)
+}  
+
+#######################################
+## min med and max
+get.min.med.max <- function(x){
+  val <- c("","","")
+  val[1] <- min(x,na.rm = T)
+  val[2] <- max(x,na.rm = T)
+  val[3] <- (as.numeric(val[1]) + as.numeric(val[2]))/2
+  return(as.numeric(val))
+}
+
+#######################################
+## Compute barycenters
+get.bary <- function(data,type="ori",rad=2500,max_n.cl=3){
+  
+  # data = d1
+  # type="ori"
+  # rad=2500
+  # max_n.cl = 1
+  # 
+  ## Quel type de points
+  if( type == "ori" ){
+    x <- "xlonori"
+    y <- "ylatori"
+  }
+  if( type == "des" ){
+    x <- "xlondes"
+    y <- "ylatdes"
+  }
+  
+  ## Chicoutimi
+  x.min <<- min(c(data$xlondes,data$xlonori))
+  x.max <<- max(c(data$xlondes,data$xlonori))
+  y.min <<- min(c(data$ylatdes,data$ylatori))
+  y.max <<- max(c(data$ylatdes,data$ylatori))
+  
+  ## One degree for the latitude of Saguenay (48.41) in meter
+  lat2m <- 111198.26
+  lon2m <- 74032.11
+  rad.lat <- rad / lat2m
+  rad.lon <- rad / lon2m
+  
+  ## Grid
+  grid.lat <<- seq(min(data[,y]),max(data[,y])+rad.lat,by=rad.lat)
+  grid.lon <<- seq(min(data[,x]),max(data[,x])+rad.lon,by=rad.lon)
+  
+  ## Info sur les barycentres
+  data$cl.id  <- NA
+  data$cl.lon <- NA
+  data$cl.lat <- NA
+  
+  ## Calcul des barycentes
+  set.seed(1)
+  n.cl.tot <- 0
+  for( i in 2:length(grid.lat)){
+    for( j in 2:length(grid.lon)){
+      
+      ## Selectionne la région
+      d2 <- data %>% filter(
+        get(y,envir=as.environment(data)) >= grid.lat[i-1] & 
+          get(y,envir=as.environment(data)) < grid.lat[i] & 
+          get(x,envir=as.environment(data)) >= grid.lon[j-1] & 
+          get(x,envir=as.environment(data)) < grid.lon[j])
+      
+      ## Nombre de points distincts
+      n.distinct <- d2 %>% 
+        select(one_of(c(x,y))) %>% 
+        distinct(get(x,envir=as.environment(d2)),get(y,envir=as.environment(d2))) %>% 
+        nrow()
+      
+      ## Garde les régions avec un minimum de points distintcs
+      if( nrow(d2) == 0 | n.distinct == 0) next
+      if( nrow(d2) > max_n.cl & n.distinct > max_n.cl ) n.cl <- max_n.cl else n.cl <- 1
+      
+      ## Algo de clustering
+      km <- kmeans(cbind(d2[,y], d2[,x]), centers = n.cl, iter.max = 100 )
+      
+      ## Coord des clusters
+      km.coord <- as.data.frame(km$centers)
+      
+      ## ID du cluster
+      id <- n.cl.tot + 1
+      
+      ## Ajoute les info sur les clusters
+      data[which(data$cledeplacement %in% d2$cledeplacement),"cl.id"] <- id
+      data[which(data$cledeplacement %in% d2$cledeplacement),"cl.lat"] <- km.coord$V1
+      data[which(data$cledeplacement %in% d2$cledeplacement),"cl.lon"] <- km.coord$V2
+      
+      ## Nombre de clusters
+      n.cl.tot <- n.cl.tot + 1
+    }
+  }
+  
+  ## Output
+  return(data)
+}
+
+
+
+
+
+######################################
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+####################################
+## Create a map of the flux
+create.map <- function(data, cond){
+  
+  ## Sélection sur la période
+  if( cond == "24h") d2 <- data
+  if( cond == "6-9") d2 <- filter(data, ghredep == 1) 
+  if( cond == "9-15") d2 <- filter(data, ghredep == 2) 
+  if( cond == "15-18") d2 <- filter(data, ghredep == 3) 
+  if( cond == "18-24") d2 <- filter(data, ghredep == 4) 
+  if( cond == "24-6") d2 <- filter(data, ghredep == 5) 
+  
+  d1.freq.top <- d2 %>% 
+    group_by(cl.ori,cl.des) %>% 
+    dplyr::summarise(freq=sum(facdep)) %>% 
+    arrange(desc(freq)) %>% 
+    head(n.voyages) %>%
+    inner_join(d1.ori.cl,by="cl.ori") %>%
+    inner_join(d1.des.cl,by="cl.des") 
+  
+  ## Clusters d'origine
+  map1 <- addCircles(map.city,
+                     d1.freq.top$cl.lon.ori,
+                     d1.freq.top$cl.lat.ori,
+                     stroke=FALSE,
+                     color="black",
+                     radius=40,
+                     fillOpacity=1, 
+                     label=paste0("Origine ",d1.freq.top$cl.ori),
+                     group = "origine (bary)") 
+  
+  ## Clusters de destination
+  map1 <- addCircles(map1,d1.freq.top$cl.lon.des,
+                     d1.freq.top$cl.lat.des,
+                     stroke=FALSE,
+                     color="darkorange",
+                     radius=40,
+                     fillOpacity=1, 
+                     label=paste0("Destination ",d1.freq.top$cl.des),
+                     group = "destination (bary)") 
+  
+  ## Boucle sur les plus gros flux
+  for(i in 1:nrow(d1.freq.top)){
+    i.lat <- as.numeric(d1.freq.top[i, c("cl.lat.ori", "cl.lat.des")])
+    i.lon <- as.numeric(d1.freq.top[i, c("cl.lon.ori", "cl.lon.des")])
+    i.freq <- as.numeric(round(d1.freq.top[i,"freq"],digits=0))
+    
+    i.ori <- d1.freq.top[i,"cl.ori"]
+    i.des <- d1.freq.top[i,"cl.des"]
+    ## Couleur 2 tons, au dessus/dessous de la moyenne
+    i.col <- ifelse(i.freq <= 50, "blue", 
+                    ifelse(i.freq>50 & i.freq<=100,"green",
+                           ifelse(i.freq>100 & i.freq<=150,"orange",
+                                  ifelse(i.freq>150 & i.freq<=200,"red",
+                                         ifelse(i.freq>200,"purple","black")))))
+    map1 <- addPolylines(map1, lat = i.lat, 
+                         lng = i.lon,
+                         weight=5,
+                         color = i.col,
+                         highlightOptions = highlightOptions(color = "grey", weight = 8,bringToFront = TRUE),
+                         label=paste0(i.ori," -> ",i.des," (",i.freq,")"),
+                         group="Flux"
+    )
+  }
+  return(map1)
+}
---
+++ R/polygon_decoupageCivilia.R
@@ -0,0 +1,223 @@
+
+
+####################################
+plot.decoupage.Civilia <- function(){
+  
+  ## Saguenay GTFS
+  stops <- fread("/Users/lavieestuntoucan/Civilia/projets/Saguenay/data/gtfs-static/gtfs-2017/stops.txt")
+  routes <- fread("/Users/lavieestuntoucan/Civilia/projets/Saguenay/data/gtfs-static/gtfs-2017/shapes.txt")
+
+    ## Carte satellite
+  map.city <- leaflet() %>% addProviderTiles('Esri.WorldImagery') %>% 
+    setView(coord$lon, coord$lat, zoom = 11)
+  
+  ## Zones
+  for( i in 1:length(zone.list)){
+    zone.df <- zone.list[[i]]
+    if( i == 1 ) which.map <- map.city else which.map <- map1
+    map1 <- addPolygons(which.map, 
+                        lng=zone.list[[i]]$lon,
+                        lat=zone.list[[i]]$lat,
+                        opacity=1,
+                        highlightOptions = highlightOptions(color = "grey", 
+                                                            weight = 8,
+                                                            bringToFront = TRUE),
+                        label=comment(zone.list[[i]]),
+                        group="Zones")
+  }
+  
+  ## Points
+  for( i in 1:nrow(coins)){
+    map1 <- addPopups(map1,
+                      coins[i,]$lon,
+                      coins[i,]$lat,
+                      as.factor(i),
+                      options = popupOptions(minWidth = 10, closeOnClick = FALSE, closeButton = FALSE),
+                      group="Points de construction")
+  }
+  
+  ## Add the gtfs 2017
+  ## Lines
+  for( rte in unique(routes$shape_id) ){
+    shape <- routes %>% filter(shape_id == rte)  %>% distinct()
+    map1 <- addPolylines(map1,
+                         shape$shape_pt_lon,
+                         shape$shape_pt_lat,
+                         color = "orange",
+                         opacity=1,
+                         group = "Lignes de bus"
+    )
+  }
+  
+  ## Stops
+  map1 <- addCircles(map1,
+                     stops$stop_lon,
+                     stops$stop_lat,
+                     radius=20,
+                     group="Arrêts de bus",
+                     label=stops$stop_name,
+                     color="red",
+                     highlightOptions = highlightOptions(weight = 20,
+                                                         col="darkorange",
+                                                         bringToFront = TRUE)
+  )
+  
+  ## Layer control
+  map1 <- addLayersControl(map1,
+                           overlayGroups = c("Zones","Arrêts de bus","Lignes de bus","Points de construction"),
+                           options = layersControlOptions(collapsed = FALSE)) %>% hideGroup(c("Points de construction","Arrêts de bus","Lignes de bus"))
+  
+  ## Print map
+  map1
+}
+
+####################################
+create.polygone <- function(){
+  ## Coins des polygones
+  coins <<- data.frame(lat = rep(x=NA,49), lon = rep(x=NA,49))
+  coins[1,] <<- c(48.43008935405231, -71.07193152957763)
+  coins[2,] <<- c(48.42593172234239, -71.0720173602661)
+  coins[3,] <<- c(48.424821064525354,-71.08060042911376)
+  coins[4,] <<- c(48.41661853231392, -71.09553496890868)
+  coins[5,] <<- c(48.421375022984016,-71.10733668857421)
+  coins[6,] <<- c(48.424166049819746, -71.10673587375487)
+  coins[7,] <<- c(48.425960200428804, -71.09669368320311)
+  coins[8,] <<- c(48.434673745351155, -71.09553496890868)
+  coins[9,] <<- c(48.43401885763197, -71.08789603763427)
+  coins[10,] <<- c(48.42527672195053, -71.0660735850891)
+  coins[11,] <<- c(48.42707795271598, -71.06296222263182)
+  coins[12,] <<- c(48.43048089430781, -71.06283347659911)
+  coins[13,] <<- c(48.42934185973257, -71.0407320743164)
+  coins[14,] <<- c(48.42401653441017, -71.04223411136473)
+  coins[15,] <<- c(48.4260100370417, -71.05476539188231)
+  coins[16,] <<- c(48.430110710871396, -71.03382270389403)
+  coins[17,] <<- c(48.43534997924627, -71.02420966678466)
+  coins[18,] <<- c(48.43096497627119, -71.01339500003661)
+  coins[19,] <<- c(48.4155860017496, -71.02412383609618)
+  coins[20,] <<- c(48.416326577522184, -71.03073279910888)
+  coins[21,] <<- c(48.42304823349079, -71.03124778323973)
+  coins[22,] <<- c(48.41857672233808, -71.04442279392089)
+  coins[23,] <<- c(48.41213933411564, -71.05103175693358)
+  coins[24,] <<- c(48.417095625560435, -71.05545203739013)
+  coins[25,] <<- c(48.420855248508126, -71.05987231784667)
+  coins[26,] <<- c(48.42209415424879,-71.07300441318358)
+  coins[27,] <<- c(48.41785042065619,-71.0775105243286)
+  coins[28,] <<- c(48.41340693739168,-71.06738250308837)
+  coins[29,] <<- c(48.41060503821868,-71.07035670089721)
+  coins[30,] <<- c(48.40764233704221,-71.07295307922362)
+  coins[31,] <<- c(48.40457974815759,-71.07376847076415)
+  coins[32,] <<- c(48.4076565812108,-71.0928872566223)
+  coins[33,] <<- c(48.404038434199634,-71.09709296035766)
+  coins[34,] <<- c(48.40671645752666,-71.10327276992797)
+  coins[35,] <<- c(48.41076171472542,-71.10104117202758)
+  coins[36,] <<- c(48.3979126379663,-71.10361609268188)
+  coins[37,] <<- c(48.393495908634875,-71.07619318771361)
+  coins[38,] <<- c(48.39566157866855,-71.07426199722289)
+  coins[39,] <<- c(48.407033293709155,-71.06202475764161)
+  coins[40,] <<- c(48.40441226739853,-71.0521542284668)
+  coins[41,] <<- c(48.40501055704052,-71.04545943476563)
+  coins[42,] <<- c(48.41042333391143,-71.04327075220948)
+  coins[43,] <<- c(48.4105942543238,-71.04764811732178)
+  coins[44,] <<- c(48.4005731118742,-71.06755437409669)
+  coins[45,] <<- c(48.39250908008252,-71.04511469990842)
+  coins[46,] <<- c(48.39011529110962,-71.0536119380676)
+  coins[47,] <<- c(48.39111271686701,-71.06041402012937)
+  coins[48,] <<- c(48.39627052094977,-71.05603665501707)
+  coins[49,] <<- c(48.432524,-71.051009)
+  
+  ## zone 16
+  zone16 <<- c(1,2,3,4,5,6,7,8,9)
+  zone16 <<- coins[zone16,]
+  comment(zone16) <<- "zone16"
+  
+  ## zone 11
+  zone11 <<- c(3,2,26,27,32,33,34,35,4)
+  zone11 <<- coins[zone11,]
+  comment(zone11) <<- "zone11"
+  
+  ## zone Term
+  zoneTerm <<- c(1,12,11,10,2)
+  zoneTerm <<- coins[zoneTerm,]
+  comment(zoneTerm) <<- "zoneTerm"
+  
+  ## zone 14a
+  zone14a <<- c(2,10,11,15,25,26)
+  zone14a <<- coins[zone14a,]
+  comment(zone14a) <<- "zone14a"
+  
+  ## zone 14b
+  zone14b <<- c(26,25,28,27)
+  zone14b <<- coins[zone14b,]
+  comment(zone14b) <<- "zone14b"
+  
+  ## zone 14c
+  zone14c <<- c(27,28,29,30,31,32)
+  zone14c <<- coins[zone14c,]
+  comment(zone14c) <<- "zone14c"
+  
+  ## zone 2a
+  zone2a <<- c(32,31,38,37,36,33)
+  zone2a <<- coins[zone2a,]
+  comment(zone2a) <<- "zone2a"
+  
+  ## zone Hop
+  zoneHop <<- c(12,49,13,14,15,11)
+  zoneHop <<- coins[zoneHop,]
+  comment(zoneHop) <<- "zoneHop"
+  
+  ## zone Uni
+  zoneUni <<- c(15,14,22,23,24,25)
+  zoneUni <<- coins[zoneUni,]
+  comment(zoneUni) <<- "zoneUni"
+  
+  ## zone 5b
+  zone5b <<- c(25,28,29,39,23,24)
+  zone5b <<- coins[zone5b,]
+  comment(zone5b) <<- "zone5b"
+  
+  ## zone 15b
+  zone15b <<- c(29,39,44,38,31,30)
+  zone15b <<- coins[zone15b,]
+  comment(zone15b) <<- "zone15b"
+  
+  ## zone 12
+  zone12 <<- c(13,16,17,18,19,20,21,14)
+  zone12 <<- coins[zone12,]
+  comment(zone12) <<- "zone12"
+  
+  ## zone 10
+  zone10 <<- c(14,21,20,42,43,23,22)
+  zone10 <<- coins[zone10,]
+  comment(zone10) <<- "zone10"
+  
+  ## zone 2d
+  zone2d <<- c(23,43,40,39)
+  zone2d <<- coins[zone2d,]
+  comment(zone2d) <<- "zone2d"
+  
+  ## zone 2c
+  zone2c <<- c(44,39,40,48)
+  zone2c <<- coins[zone2c,]
+  comment(zone2c) <<- "zone2c"
+  
+  ## zone 2b
+  zone2b <<- c(44,48,47,37,38)
+  zone2b <<- coins[zone2b,]
+  comment(zone2b) <<- "zone2b"
+  
+  ## zone 5a
+  zone5a <<- c(40,41,42,43)
+  zone5a <<- coins[zone5a,]
+  comment(zone5a) <<- "zone5a"
+  
+  ## zone 15a
+  zone15a <<- c(40,41,45,46,47,48)
+  zone15a <<- coins[zone15a,]
+  comment(zone15a) <<- "zone15a"
+  
+  ############
+  ## Loop sur les zones
+  zone.list <<- list(zone16,zone11,zoneTerm,zone14a,zone14b,zone14c,zone2a,zoneHop,zoneUni,zone5b,zone15b,zone12,zone10,zone2d,zone2c,zone2b,zone5a,zone15a)
+  zone.names <<- sapply(zone.list,comment)
+  
+}
